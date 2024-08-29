cd30a1f238fb0e25bdec3525ce716fd4d0674108
Ameya Kale <ameya.k.kale@gmail.com>
2024-08-28 23:34:53 -0400

    Added Why Choose Us Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUS from "@/components/WhyChooseUS";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection></HeroSection>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUS />
     </main>
     
   );
